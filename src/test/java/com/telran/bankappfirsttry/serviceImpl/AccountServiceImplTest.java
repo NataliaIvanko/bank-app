90caaec4ac1f51b6bc2fd41d2d94e09516457aac
Natalia <natalie0505@yandex.ru>
2023-02-14 11:00:12 +0100

    converter enums

--- src/test/java/com/telran/bankappfirsttry/serviceImpl/AccountServiceImplTest.java
+++ src/test/java/com/telran/bankappfirsttry/serviceImpl/AccountServiceImplTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Nested;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -32,73 +33,81 @@ public class AccountServiceImplTest {
     AccountMapper mapper;
     @InjectMocks
     AccountServiceImpl service;
-    @Test
-    @DisplayName("should save() account")
-    public void shouldSaveAccount(){
-        AccountRequestDTO request = AccountRequestDTO.builder()
-                .firstName("Jane")
-                .lastName("Atkins")
-                .country("Germany")
-                .city("Berlin")
-                .email("ja@gmail.com")
-                .creationDate(Instant.now())
-                .balance(100F)
-                .build();
-        Account account = Account.builder()
-                        .userId(1L)
-                        .firstName(request.getFirstName())
-                        .lastName(request.getLastName())
-                        .country(request.getCountry())
-                        .city(request.getCity())
-                        .email(request.getEmail())
-                        .creationDate(request.getCreationDate())
-                        .balance(request.getBalance())
-                .build();
 
-        Mockito
-                .when(accountRepository.findById(account.getUserId()))
-                       .thenReturn(Optional.of(account));
+    @Nested
+    @DisplayName("createAccount()")
+    class createAccount {
+        @Test
+        @DisplayName("should throw 404 NOT_FOUND when no such account")
+        public void shouldThrow404WhenNoSuchAccount() {
 
-        Mockito
-                .when(accountRepository.save(ArgumentMatchers.argThat(
-                        savedAccount ->{
-                            return savedAccount.equals(request.getFirstName())
-                                    && savedAccount.getLastName().equals(request.getLastName())
-                                    && savedAccount.getCountry().equals(request.getCountry())
-                                    && savedAccount.getCity().equals(request.getCity())
-                                    && savedAccount.getEmail().equals(request.getEmail())
-                                    && savedAccount.getCreationDate().equals(request.getCreationDate())
-                                    && savedAccount.getBalance().equals(request.getBalance());
-                        }
-                )))
-                .thenReturn(account);
+            Long accountId = 1L;
+            HttpStatus expectedStatus = HttpStatus.NOT_FOUND;
+            String expectedMessage = "account is not found";
+
+            Mockito
+                    .when(accountRepository.findById(accountId))
+                    .thenReturn(Optional.empty());
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class,
+                    () -> service.getAccountById(accountId)
+            );
+            Assertions.assertEquals(expectedStatus, exception.getStatus());
+            Assertions.assertEquals(expectedMessage, exception.getReason());
 
 
-        service.createAccount(request);
+        }
+
+        @Test
+        @DisplayName("should save() account")
+        public void shouldSaveAccount() {
+            AccountRequestDTO request = AccountRequestDTO.builder()
+                    .firstName("Jane")
+                    .lastName("Atkins")
+                    .country("Germany")
+                    .city("Berlin")
+                    .email("ja@gmail.com")
+                    .creationDate(Instant.now())
+                    .balance(100F)
+                    .build();
+            Account account = Account.builder()
+                    .userId(1L)
+                    .firstName(request.getFirstName())
+                    .lastName(request.getLastName())
+                    .country(request.getCountry())
+                    .city(request.getCity())
+                    .email(request.getEmail())
+                    .creationDate(request.getCreationDate())
+                    .balance(request.getBalance())
+                    .build();
+
+            Mockito
+                    .when(accountRepository.findById(account.getUserId()))
+                    .thenReturn(Optional.of(account));
+
+            Mockito
+                    .when(accountRepository.save(ArgumentMatchers.argThat(
+                            savedAccount -> {
+                                return savedAccount.equals(request.getFirstName())
+                                        && savedAccount.getLastName().equals(request.getLastName())
+                                        && savedAccount.getCountry().equals(request.getCountry())
+                                        && savedAccount.getCity().equals(request.getCity())
+                                        && savedAccount.getEmail().equals(request.getEmail())
+                                        && savedAccount.getCreationDate().equals(request.getCreationDate())
+                                        && savedAccount.getBalance().equals(request.getBalance());
+                            }
+                    )))
+                    .thenReturn(account);
+
+
+            service.createAccount(request);
+        }
     }
-
-
-
-    @Test
-    @DisplayName("should throw 404 NOT_FOUND when no such account")
-    public void shouldThrow404WhenNoSuchAccount(){
-
-        Long accountId = 1L;
-        HttpStatus expectedStatus = HttpStatus.NOT_FOUND;
-        String expectedMessage = "account is not found";
-
-        Mockito
-                .when(accountRepository.findById(accountId))
-                .thenReturn(Optional.empty());
-        ResponseStatusException exception = Assertions.assertThrows(
-                ResponseStatusException.class,
-                ()->service.getAccountById(accountId)
-        );
-        Assertions.assertEquals(expectedStatus, exception.getStatus());
-        Assertions.assertEquals(expectedMessage, exception.getReason());
-
-
-    }
-
 
 }
+
+
+
+
+
+
